f09e86fe20b3f8166ba73c97225b3f789ac261d1
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-09 17:21:06 -0500

    add user Icon to display next to name

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Post.css';
+import userIcon from '../../images/user-icon.png'
 
 export function Post(props){
     const loadingDiv = <h3>Loading...</h3>;
@@ -27,7 +28,10 @@ export function Post(props){
                 <a rel="noreferrer" href={props.post.url} target='_blank'><h3>{props.post ? props.post.title : loadingDiv}</h3></a>
                 {isImage && <img src={props.post.url}/>}
                 <div className="postDetails">
-                    <h5>{props.post.author}</h5>
+                    <div className="author">
+                        <img className="icon" src={userIcon}/>
+                        <h5>{props.post.author}</h5>
+                    </div>
                     <h5>{props.post.created_utc}</h5>
                     <h5>{props.post.num_comments} comments</h5>
                 </div>
