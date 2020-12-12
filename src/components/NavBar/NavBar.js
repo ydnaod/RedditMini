8ba92dcf1755c42f5ed263df91d3f3dac2611067
ydnaod <65431123+ydnaod@users.noreply.github.com>
2020-12-12 12:35:43 -0500

    add color to logo in navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
-import {SearchBar} from '../SearchBar/SearchBar'
+import { SearchBar } from '../SearchBar/SearchBar'
 import './NavBar.css'
+import redditLogo from '../../images/reddit-logo.png'
 
-export function NavBar(props){
+export function NavBar(props) {
     return (
         <div className='NavBar'>
-            <h1 className='brand'>RedditMini</h1>
+            <div className="logo">
+                <img className = "image" src={redditLogo}/>
+                <h1 className='brand'>Reddit<span className="orange">Mini</span></h1>
+            </div>
             <SearchBar searchResults={props.searchResults}
-                handleSearchChange={props.handleSearchChange}/>
+                handleSearchChange={props.handleSearchChange} />
             <div></div>
         </div>
     );
